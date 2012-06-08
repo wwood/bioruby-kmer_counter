2f2fefbcbabec189d28a062ed6085ff29456ab16
Ben J Woodcroft <gmail.com after donttrustben>
2012-06-08 11:56:39 +1000

    in-code documentation

--- lib/bio-kmer_counter/kmer_counter.rb
+++ lib/bio-kmer_counter/kmer_counter.rb
@@ -1,4 +1,4 @@
-# Initialise the hash of the different
+
 module Bio
   class Sequence
     class NA
@@ -12,6 +12,8 @@ module Bio
     end
 
     class Kmer
+      # Return a hash of Strings to 0, for each kmer of length k. For instance
+      # empty_full_kmer_hash(1) => {'A'=>0, 'T'=>0, 'C'=>0, 'G'=>0}
       def self.empty_full_kmer_hash(k=4)
         return @empty_full_hash.dup unless @empty_full_hash.nil?
         
@@ -29,6 +31,13 @@ module Bio
         counts
       end
       
+      # Take a kmer hash, and merge those keys to the lowest lexigraphical form
+      # (See Bio::Sequence::NA#lowest_lexigraphical_form for what this means)
+      # When 2 keys are reverse complements they get merged into one hash entry, 
+      # where the key is the lowest_lexigraphical_form of the two and the
+      # value is the sum of the original 2 values
+      # 
+      # For instance {'A'=>2,'T'=>5} #=> {'A'=>7}  
       def self.merge_down_to_lowest_lexigraphical_form(hash)
         keys = empty_full_kmer_hash.keys
         
